ffaab22ba5ad0a3cc7d5687d2738a0b5adf64a1d
Tamannarahman <tamannarahmanmridha@gmail.com>
2023-01-19 17:02:13 +0600

    mongodb added

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
 
 const app = express();
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.m5hswga.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.log);
+
+
+
+
 
 app.get('/', async (req, res) => {
     res.send('deplefy server is running');
