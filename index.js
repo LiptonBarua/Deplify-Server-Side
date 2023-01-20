1d254e6c197099162274ae687f2d8a4771225711
Lipton Barua <liptonbarua274@gmail.com>
2023-01-20 11:22:12 +0600

    project

--- index.js
+++ index.js
@@ -1,20 +1,44 @@
+
 const express = require('express');
-const cors = require('cors');
-const port = process.env.PORT || 5000;
-
-const app = express();
-
-// middleware
-app.use(cors());
+const http= require("http")
+const app= express();
+const cors = require("cors")
+const port =5000 || process.env.PORT;
+app.use(cors())
 app.use(express.json());
 
-app.get('/', async (req, res) => {
+const httpServer= http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(httpServer, {
+    cors: {
+        origin: '*',
+        methods: ["GET", "POST"],
+    }
+})
+
+io.on("connection", (socket) => {
+  console.log("user is connected");
+
+  socket.on("disconnection", (socket) => {
+ console.log("user disconnected")
+});
+
+// socket.on("chatEvent", (data) => {
+//     console.log(data)
+//     io.emit("chatShow", data);
+// });
+socket.on("reactEvent", (data) => {
+    console.log(data)
+    socket.broadcast.emit("showMessage", data)
+   
+});
+});
+
+
+app.get('/', async(req, res) => {
     res.send('deplefy server is running');
 })
 
-
-app.get('team', async (req, res)=>{
-    res.send('Team Tech Airme')
+httpServer.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
-
-app.listen(port, () => console.log(`deplefy running on ${port}`));
