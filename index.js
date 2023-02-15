e6505d87ef9ebae9ae9d3e4d7ed85a856852a802
Lipton Barua <liptonbarua274@gmail.com>
2023-02-15 11:17:08 +0600

    socket.io code remove

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 const express = require('express');
-const http = require("http")
+// const http = require("http")
 const app = express();
 const cors = require("cors")
 const jwt = require('jsonwebtoken');
@@ -11,29 +11,29 @@ app.use(cors())
 app.use(express.json());
 require('dotenv').config();
 
-const httpServer = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(httpServer, {
-    cors: {
-        origin: '*',
-        methods: ["GET", "POST"],
-    }
-})
+// const httpServer = http.createServer(app);
+// const { Server } = require("socket.io");
+// const io = new Server(httpServer, {
+//     cors: {
+//         origin: '*',
+//         methods: ["GET", "POST"],
+//     }
+// })
 
-io.on("connection", (socket) => {
-    console.log("user is connected");
+// io.on("connection", (socket) => {
+//     console.log("user is connected");
 
-    socket.on("disconnection", (socket) => {
-        console.log("user disconnected")
-    });
+//     socket.on("disconnection", (socket) => {
+//         console.log("user disconnected")
+//     });
 
 
-    socket.on("reactEvent", (data) => {
-        console.log(data)
-        socket.broadcast.emit("showMessage", data)
+//     socket.on("reactEvent", (data) => {
+//         console.log(data)
+//         socket.broadcast.emit("showMessage", data)
 
-    });
-});
+//     });
+// });
 
 app.get('/', async (req, res) => {
     res.send('deplefy server is running');
@@ -334,6 +334,6 @@ run().catch(console.dir);
 
 
 
-httpServer.listen(port, () => {
+app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
